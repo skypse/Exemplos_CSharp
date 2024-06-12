c3b44b26db332bb0f917c3badafc33c97a76f64f
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-12 12:30:02 -0300

    adicionando exercicio 31,32

---
+++ Exercicio_31/Pratica_31.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_31
+{
+    internal class Pratica_31
+    {
+        public static void Main(string[] args)
+        {
+            int contadora = 15;
+            do
+            {
+                Console.WriteLine(contadora * contadora);
+                contadora++;
+            } while (contadora < 201);
+        }
+    }
+}
---
+++ Exercicio_32/Pratica_32.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_32
+{
+    internal class Pratica_32
+    {
+        public static void Main(string[] args)
+        {
+            int contadora = 1;
+            int acumuladora = 0;
+            do
+            {
+                if (contadora % 2 == 0) 
+                {
+                    acumuladora += contadora;
+                }
+                contadora++;
+            } while (contadora < 501);
+            Console.WriteLine("A soma dos números pares é: " + acumuladora);
+        }
+    }
+}
