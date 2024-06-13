50e5dbb5b26d4a03aed3508299c68b2cf5274eba
skypse <skypse0136@gmail.com>
2024-06-13 10:50:44 -0300

    adicionando exercicio 41 e 42

---
+++ Exercicio_41/Pratica_41.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_41
+{
+    internal class Pratica_41
+    {
+        public static void Main(string[] args)
+        {
+            string[] nomes = { "Jailson", "Amaral", "Johny Dudu", "isaac" };
+            foreach (string s in nomes)
+            {
+                if (s == "isaac")
+                {
+                    break;
+                }
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Exercicio_42/Pratica_42.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_42
+{
+    internal class Pratica_42
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
