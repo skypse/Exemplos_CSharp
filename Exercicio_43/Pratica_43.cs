fe2f51021c0e982c00c9dd89589113790ac0cca8
skypse <skypse0136@gmail.com>
2024-06-13 11:54:40 -0300

    exercicio 43 e 44

---
+++ Exercicio_43/Pratica_43.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_43
+{
+    internal class Pratica_43
+    {
+        public static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+                if (i == 4)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Exercicio_44/Pratica_44.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_44
+{
+    internal class Pratica_44
+    {
+        public static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                if (i == 4) 
+                {
+                    i++;
+                    continue;
+                }
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
