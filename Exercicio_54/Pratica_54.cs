1c36029ba3962ad0cc13e4523bde03365ef926d7
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-17 12:02:05 -0300

    adicionando exercicio 54, praticado em aula

---
+++ Exercicio_54/Pratica_54.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_54
+{
+    internal class Pratica_54
+    {
+        public static void Main(string[] args)
+        {
+            // Exemplo Matriz (Array 2D)
+            int[,] numeros = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 } };
+            Console.WriteLine(numeros[0, 2]); // output = 3
+        }
+    }
+}
