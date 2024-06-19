7ca729b330b302440db5dee22c6ca02cd02d58bc
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-19 12:41:13 -0300

    adicionando exercicio 68

---
+++ Exercicio_68/Pratica_68.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_68
+{
+    internal class Pratica_68
+    {
+        // utilizando a sobrecarga
+        public static void Main(string[] args)
+        {
+            int meuNumero01 = Somar(8, 5);
+            double meuNumero02 = Somar(2.5, 5.5);
+            Console.WriteLine($"Int: {meuNumero01}");
+            Console.WriteLine($"Double: {meuNumero02}");
+        }
+        static int Somar(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double Somar(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
