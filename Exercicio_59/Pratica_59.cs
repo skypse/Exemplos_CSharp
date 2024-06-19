2f471296a849b3fe2b3090a87eb7333e73c4988d
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-19 10:00:21 -0300

    adicionando exercicio 59

---
+++ Exercicio_59/Pratica_59.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_59
+{
+    internal class Pratica_59
+    {
+        public static void Main(string[] args)
+        {
+            Equipe("isaac", 2);
+            Equipe("gean", 2);
+            Equipe("carlinhos", 2);
+        }
+
+        static void Equipe(string nome, int numeroEquipe)
+        {
+            Console.WriteLine($"{nome} pertence a equipe {numeroEquipe}");
+        }
+    }
+}
