a9e15abe67e65a628915f789987d039b690d822e
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-19 13:50:50 -0300

    adicionando exercicios 69 e 70

---
+++ Exercicio_69/Carro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_69
+{
+    internal class Carro
+    {
+        string cor = "Azul";
+
+        public static void Main(string[] args)
+        {
+            Carro fusca = new Carro();
+            Console.Write(fusca.cor);
+        }
+    }
+}
---
+++ Exercicio_70/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_70
+{
+    internal class Aluno
+    {
+        string nome = "Freitas";
+        int idade = 19;
+        string habilidade = "Jogar um mine";
+        bool louco = true;
+
+        public static void Main(string[] args)
+        {
+            Aluno freitas = new Aluno();
+            Console.WriteLine(freitas.habilidade);
+
+            Aluno isaac = new Aluno();
+            isaac.nome = "isaac";
+            isaac.habilidade = "Jogar vava";
+            Console.WriteLine(isaac.habilidade);
+        }
+    }
+}
