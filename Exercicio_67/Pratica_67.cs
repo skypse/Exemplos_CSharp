c06cdcc8958406430c1f9bc1191f1cc40c991899
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-19 11:06:13 -0300

    adicionando exercicio 67

---
+++ Exercicio_67/Pratica_67.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_67
+{
+    internal class Pratica_67
+    {
+        public static void Main(string[] args)
+        {
+            Aluno(aluno02: "Freitas H", aluno01: "Gabriel H", aluno03: "isaac");
+        }
+        static void Aluno(string aluno01, string aluno02, string aluno03)
+        {
+            Console.WriteLine($"{aluno02} ta jogando mine em aula!");
+            /*Console.WriteLine($"{aluno03} joga bastante TFT");
+            Console.WriteLine($"{aluno01} joga bastante Just Cause");*/
+        }
+    }
+}
