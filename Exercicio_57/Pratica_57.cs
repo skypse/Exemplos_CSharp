a4473270b16d65e97d63986a37fb122a5b956aec
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-19 09:42:03 -0300

    adicionando exercicio 57 e 58 praticados em sala de aula

---
+++ Exercicio_57/Pratica_57.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_57
+{
+    internal class Pratica_57
+    {
+        static void MeuMetodo()
+        {
+            Console.WriteLine("Testando");
+        }
+        static void Main(string[] args)
+        {
+            MeuMetodo();
+        }
+    }
+}
---
+++ Exercicio_58/Pratica_58.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_58
+{
+    internal class Pratica_58
+    {
+        static void MeuMetodo(string fname)
+        {
+            Console.WriteLine($"{fname} Amaral");
+        }
+
+        static void Main(string[] args)
+        {
+            MeuMetodo("Gabriel");
+            MeuMetodo("Gean");
+            MeuMetodo("isaac");
+        }
+    }
+}
