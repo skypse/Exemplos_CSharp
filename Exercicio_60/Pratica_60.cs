948d46118f2016a30e9aabacab4eeea0dab831a3
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-19 10:07:39 -0300

    adicionando exercicio 60

---
+++ Exercicio_60/Pratica_60.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_60
+{
+    internal class Pratica_60
+    {
+        static void Main(string[] args)
+        {
+            Game();
+            Game("Isaac", "R6");
+        }
+        static void Game(string pessoaNome = "Guilherme Freitas", string jogos = "Minecraft")
+        {
+            Console.WriteLine($"{pessoaNome} joga {jogos}");
+        }
+    }
+}
