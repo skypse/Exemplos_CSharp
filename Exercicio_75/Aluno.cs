15e2363e3cf894e81baf0b94909b0ae9fd0f9ccd
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-24 10:46:48 -0300

    exemplo construtor 05

---
+++ Exercicio_75/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_75
+{
+    class Aluno
+    {
+        public string nome;
+        public string idade;
+        public string jogoFavorito;
+    }
+}
---
+++ Exercicio_75/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_75
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Aluno Freitas = new Aluno();
+            Freitas.nome = "Freitas";
+            Freitas.idade = "19";
+            Freitas.jogoFavorito = "Minecraft";
+
+            Aluno Isaac = new Aluno();
+            Isaac.nome = "Isaac";
+            Isaac.idade = "18";
+            Isaac.jogoFavorito = "R6";
+
+            Console.WriteLine($"Nome: {Isaac.nome} Idade:{Isaac.idade}  Jogo Favorito:{Isaac.jogoFavorito}");
+            Console.WriteLine($"Nome: {Freitas.nome} Idade:{Freitas.idade}  Jogo Favorito:{Freitas.jogoFavorito}");
+        }
+    }
+}
