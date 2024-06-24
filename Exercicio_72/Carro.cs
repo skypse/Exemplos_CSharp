ea92691440f07b9085703ee5af34d1283a507d44
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-24 10:01:25 -0300

    exemplo construtor 02, praticado em sala de aula

---
+++ Exercicio_72/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_72
+{
+    class Carro
+    {
+        public string modelo;
+
+        public Carro()
+        {
+            modelo = "DOLPHIN";
+        }
+
+        public static void Main(string[] args)
+        {
+            Carro BYD = new Carro();
+            Console.WriteLine($"O modelo do carro é: {BYD.modelo}");
+        }
+    }
+}
