1a192337ba7dc40734075d602b6fe51347b1bd58
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-24 09:58:14 -0300

    exemplo construtor praticado em aula

---
+++ Exercicio_71/ExemploConstrutor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_71
+{
+    internal class ExemploConstrutor
+    {
+        public string modelo;
+
+        public ExemploConstrutor()
+        {
+            modelo = "Mustang";
+        }
+
+        public static void Main(string[] args)
+        {
+            ExemploConstrutor Ford = new ExemploConstrutor();
+            Console.WriteLine($"O modelo do carro é: {Ford.modelo}");  
+
+        }
+    }
+}
