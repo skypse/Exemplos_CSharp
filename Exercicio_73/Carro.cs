be78638ed171657482e22fb713457e27a1225399
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-24 10:33:37 -0300

    exemplo de construtor 03

---
+++ Exercicio_73/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_73
+{
+    class Carro
+    {
+        public string cor;
+
+        public Carro(string corDoCarro)
+        {
+            cor = corDoCarro;
+        }
+
+        public static void Main(string[] args)
+        {
+            Carro BYD = new Carro("Preto Fosco");
+            Console.WriteLine($"A cor do BYD é: {BYD.cor}");
+        }
+    }
+}
