5a09a4bcd5bde26305184e5f33d7e85fe7352d61
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-24 11:49:09 -0300

    exemplo utilizando o private

---
+++ Exercicio_77/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_77
+{
+    class Aluno
+    {
+        private string nome = "Amaral";
+
+        public static void Main(string[] args)
+        {
+            Aluno Amaral = new Aluno();
+            Console.WriteLine(Amaral.nome);
+        }
+    }
+}
